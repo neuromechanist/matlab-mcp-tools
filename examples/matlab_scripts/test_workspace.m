05a5918ba002b811e2c32697978de9fcea5cb63b
Seyed Yahya Shirazi <shirazi@ieee.org>
2025-02-02 08:15:48 -0800

    Add a sample workspace test script

---
+++ examples/matlab_scripts/test_workspace.m
@@ -0,0 +1,18 @@
+%% Initialize Variables
+% Create some test variables
+x = 10;
+y = 20;
+z = x + y;
+
+%% Create Arrays
+% Make some arrays
+arr1 = 1:5;
+arr2 = arr1.^2;
+
+%% Plot Data
+% Generate a simple plot
+figure;
+plot(arr1, arr2);
+title('Test Plot');
+xlabel('X');
+ylabel('Y');
